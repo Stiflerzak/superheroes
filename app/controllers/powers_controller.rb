f1a8a6871ffd12968edc9eef46cbc70057319bd0
Stiflerzak <hilarymwendia97@gmail.com>
2023-03-25 11:13:50 +0300

    Created Power method show

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,6 +2,15 @@ class PowersController < ApplicationController
     def index
         powers = Power.all 
         render json: powers, each_serializer: PowerSerializer
+    end
 
+    def show 
+        power = Power.find_by(id: params[:id])
+
+        if power
+            render json: power, serializer: PowerSerializer
+        else
+            render json: { error: "Hero not found" }, status: :not_found
+        end
     end
 end
