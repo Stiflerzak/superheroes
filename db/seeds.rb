667b83c8b1db6cb15d0769bc5ba2858712093fff
Stiflerzak <hilarymwendia97@gmail.com>
2023-03-25 09:22:29 +0300

    Added data to heroes model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,15 @@
-# db/seeds.rb
 
-# superman = Hero.create(name: "Superman")
-# spiderman = Hero.create(name: "Spider-Man")
-
-# flight = Power.create(name: "Flight")
-# super_strength = Power.create(name: "Super Strength")
-# spider_sense = Power.create(name: "Spider-Sense")
-
-# HeroPower.create(hero: superman, power: flight)
-# HeroPower.create(hero: superman, power: super_strength)
-# HeroPower.create(hero: spiderman, power: spider_sense)
+# Create some powers
+strength = Power.create(name: "Super Strength", description: "The ability to lift and move objects beyond human capability.")
+flight = Power.create(name: "Flight", description: "The ability to propel oneself through the air.")
+energy_blasts = Power.create(name: "Energy Blasts", description: "The ability to generate and emit powerful energy blasts.")
+telekinesis = Power.create(name: "Telekinesis", description: "The ability to move objects with the power of one's mind.")
+invisibility = Power.create(name: "Invisibility", description: "The ability to render oneself invisible to the naked eye.")
 
 
-5.times do
-    power = Power.create(
-        name: Faker::Superhero.power,
-        description: Faker::ChuckNorris.fact
-    ) 
-end
+# Create some heroes
+hulk = Hero.create(name: "Hulk", super_name: "The Incredible Hulk")
+thor = Hero.create(name: "Thor", super_name: "The God of Thunder")
+iron_man = Hero.create(name: "Iron Man", super_name: "The Armored Avenger")
+captain_america = Hero.create(name: "Captain America", super_name: "The First Avenger")
+black_widow = Hero.create(name: "Black Widow", super_name: "The Russian Spy")
