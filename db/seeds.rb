b9acb5449e239b5b9f9a138a9999b56f705be462
Stiflerzak <hilarymwendia97@gmail.com>
2023-03-25 09:12:30 +0300

    Added data to power model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,20 @@
 # db/seeds.rb
 
-superman = Hero.create(name: "Superman")
-spiderman = Hero.create(name: "Spider-Man")
+# superman = Hero.create(name: "Superman")
+# spiderman = Hero.create(name: "Spider-Man")
 
-flight = Power.create(name: "Flight")
-super_strength = Power.create(name: "Super Strength")
-spider_sense = Power.create(name: "Spider-Sense")
+# flight = Power.create(name: "Flight")
+# super_strength = Power.create(name: "Super Strength")
+# spider_sense = Power.create(name: "Spider-Sense")
 
-HeroPower.create(hero: superman, power: flight)
-HeroPower.create(hero: superman, power: super_strength)
-HeroPower.create(hero: spiderman, power: spider_sense)
+# HeroPower.create(hero: superman, power: flight)
+# HeroPower.create(hero: superman, power: super_strength)
+# HeroPower.create(hero: spiderman, power: spider_sense)
+
+
+5.times do
+    power = Power.create(
+        name: Faker::Superhero.power,
+        description: Faker::ChuckNorris.fact
+    ) 
+end
